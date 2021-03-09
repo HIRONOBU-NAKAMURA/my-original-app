2df0271bbf9f5a35c3cc81e64d8361f673de6db6
HIRONOBU-NAKAMURA <hironobu9dance@gmail.com>
2021-03-09 15:58:49 +0900

    seedファイルを編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true,activated: true, activated_at: Time.zone.now)
+User.create!(name: "Hironobu Nakamura", email: "hironobu9dance@gmail.com", password: "password", password_confirmation: "password", admin: true,activated: true, activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
-99.times do |n|
+50.times do |n|
   name = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@hironobunakamura.org"
   password = "password"
   User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
 
 # ユーザーの一部を対象にマイクロポストを生成する
 users = User.order(:created_at).take(6)
-50.times do
+5.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
